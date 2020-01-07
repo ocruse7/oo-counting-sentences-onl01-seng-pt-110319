1a774101f6d84bac194f4900c047a3df8e8302cb
Owen Cruse <ocruse7@gmail.com>
2020-01-07 16:39:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,7 +11,7 @@ class String
   end
 
   def question?
-    if self.end_with?("?") == true
+    if self.end_with?("?")
       true
     else
       false
@@ -20,7 +20,7 @@ class String
   end
 
   def exclamation?
-    if self.end_with?("!") == true
+    if self.end_with?("!")
       true
     else
       false
